37148084956315cb11bca397f273b8dc6743dfbe
luca-dex <luca.desano@gmail.com>
2013-02-01 01:47:37 +0100

    template added

--- hw.py
+++ hw.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,40 +12,7 @@ def hello2():
 
 @app.route("/test")
 def circle():
-    return '''
-	<!DOCTYPE html>
-	<html>
- 		<head>
-		<title>Hello, data!</title>
-    <script src="http://d3js.org/d3.v3.min.js"></script>
-  </head>
-  <body>
-
-    <div id="viz"></div>
-
-    <script type="text/javascript">
-
-    var sampleSVG = d3.select("#viz")
-        .append("svg:svg")
-        .attr("width", 100)
-        .attr("height", 100);
-
-    sampleSVG.append("svg:circle")
-        .style("stroke", "black")
-        .style("fill", "white")
-        .attr("r", 40)
-        .attr("cx", 50)
-        .attr("cy", 50)
-
-    </script>
-
-  </body>
-</html>
-
-
-
-
-    '''
+    return render_template('circle.html', name='marco')
 
 if __name__ == "__main__":
     app.run(debug=True)
