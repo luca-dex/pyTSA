19e35e6bb79ef79c9e1bdf6f0d424c6a38852014
luca-dex <luca.desano@gmail.com>
2013-02-13 14:03:11 +0100

    integrato json

--- hw.py
+++ hw.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+import json, random
 
 app = Flask(__name__)
 
@@ -12,7 +13,15 @@ def hello2():
 
 @app.route("/test")
 def circle():
-    return render_template('circle.html', name='marco')
+    return render_template('circle.html', name='mr. x')
+
+@app.route("/data")
+def circle():
+    numeretti = []
+    for _ in range(50):
+        numeretti.append([random.randint(1, 400), random.randint(1, 1000)])
+    num_to_js = json.dumps(numeretti)
+    return render_template('data.html', numb=num_to_js)
 
 if __name__ == "__main__":
     app.run(debug=True)
