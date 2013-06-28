d71bdcc50fcc3f8644ec9d39ebe4805a49ca0db9
luca-dex <luca.desano@gmail.com>
2013-06-28 13:09:03 +0200

    creato RedPanda obj con stato

--- redpanda/redpanda.py
+++ redpanda/redpanda.py
@@ -8,45 +8,123 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from dataset import *
 
-def create_dfs(path, commentstring=None, colnames=None, low_limit=-float('inf'), high_limit=float('inf')):
-    dfs = {}
+def dataset(path, commentstring=None, colnames=None, delimiter='[\s\t]+', start=-float('inf'), stop=float('inf'), \
+    colid=None, ext=None):
+    '''more than one file'''
+    
+    #microvalidation
+    if start > stop:
+        print 'maybe start > stop ? \n'
+    if colnames and colid:
+        if len(colnames) != len(colid):
+            print 'colid and colnames must have same length!'
+    if not colnames:
+        col_pref = 'Y'
+    else:
+        col_pref = None
+
+    if ext and not ext.startswith('.'):
+        ext = ''.join(('.', ext))
+
+    if colid and delimiter != ',':
+        print 'column selection work only with delimiter = \',\' (yet)'
+
+
+    dataset = {}
+
+    # skip dir, parse all file matching ext
     for x in os.listdir(path):
-        source = CommentedFile(open(os.path.join(path, x), 'rb'), \
-            commentstring=commentstring, low_limit=low_limit, high_limit=high_limit)
-        dfs[x] = pd.read_csv(source, delimiter='[\s\t]+', index_col=0, \
-            header=None, names=colnames)
-    return dfs
+        actualfile = os.path.join(path, x)
 
-def create_df(path, commentstring=None, colnames=None, low_limit=-float('inf'), high_limit=float('inf')):
+        # check if isdir
+        if os.path.isdir(actualfile):
+            continue
+        _, actualext = os.path.splitext(actualfile)
+
+        # check if ext match
+        if ext and ext != actualext:
+            continue
+
+        # import
+        source = CommentedFile(open(actualfile, 'rb'), \
+            commentstring=commentstring, low_limit=start, high_limit=stop)
+        dataset[x] = pd.read_csv(source, sep=delimiter, index_col=0, \
+            header=None, names=colnames, usecols=colid, prefix=col_pref)
+        source.close()
+
+    # return RedPanda Obj (isset = True)
+    return RedPanda(dataset, True)
+
+def timeseries(path, commentstring=None, colnames=None, delimiter='[\s\t]+', start=-float('inf'), stop=float('inf'), \
+    colid=None):
+    '''just one file'''
+    
+    # microvalidation
+    if start > stop:
+        print 'maybe start > stop ? \n'
+    if colnames and colid:
+        if len(colnames) != len(colid):
+            print 'colid and colnames must have same length!'
+    if not colnames:
+        col_pref = 'Y'
+    else:
+        col_pref = None
+
+    if colid and delimiter != ',':
+        print 'column selection work only with delimiter = \',\' (yet)' 
+
     source = CommentedFile(open(path, 'rb'), \
-        commentstring=commentstring, low_limit=low_limit, high_limit=high_limit)
-    return pd.read_csv(source, delimiter='[\s\t]+', index_col=0, header=None, names=colnames)
+        commentstring=commentstring, low_limit=start, high_limit=stop)
+    timeseries = pd.read_csv(source, sep=delimiter, index_col=0, \
+        header=None, names=colnames, usecols=colid, prefix=col_pref)
+    source.close()
 
-def get(df, l_limit, h_limit, step):
-    start = float(l_limit)
-    now = float(start + step)
-    to_return = []
-    try:
-        last_value = df.truncate(after=start).tail(1).values[0]
-    except:
-        last_value = 0
-    to_return.append(last_value)
-    while now < h_limit:
+    # return RedPanda Obj (isset = False)
+    return RedPanda(timeseries, None)
+
+class RedPanda:
+    def __init__(self, data, isSet):
+        # dataset or timeseries
+        self.data = data
+        # what's the type
+        self.isSet = isSet
+        # start with default terminal
+        self.output = ['view']
+        # range -> label:data (pandas df with index)
+        self.range = {}
+
+    def createrange(self, label, colname, start, stop, step):
+        """Select 1 column and create a range from start to stop"""
+        if not self.isSet:
+            print 'createrange works only on dataset'
+            return
+        index = np.arange(start, stop, step)
+        mean_df = pd.DataFrame(index=index)
+        for k,v in self.data.iteritems():
+            mean_df.insert(0, k, self.get(v[colname], start, stop, step))
+        self.range[label] = mean_df
+
+    @staticmethod
+    def get(df, l_limit, h_limit, step):
+        start = float(l_limit)
+        now = float(start + step)
+        to_return = []
         try:
-            last_value = df.truncate(before=start, after=now).tail(1).values[0]
+            last_value = df.truncate(after=start).tail(1).values[0]
         except:
-            pass
+            last_value = 0
         to_return.append(last_value)
-        start = start + step
-        now = now + step
-    return to_return
+        while now < h_limit:
+            try:
+                last_value = df.truncate(before=start, after=now).tail(1).values[0]
+            except:
+                pass
+            to_return.append(last_value)
+            start = start + step
+            now = now + step
+        return to_return
 
-def create_range(df_dict, colname, l_limit, h_limit, step):
-    index = np.arange(l_limit, h_limit, step)
-    mean_df = pd.DataFrame(index=index)
-    for k,v in df_dict.iteritems():
-        mean_df.insert(0, k, get(v[colname], l_limit, h_limit, step))
-    return mean_df
+
 
 def meq_relfreq(df_dict, colname, l_limit, h_limit, step, numbins=10):
     range_df = create_range(df_dict, colname, l_limit, h_limit, step)
